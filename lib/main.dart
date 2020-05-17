3423b9ad59ca778591f6c20f9518501e6179f426
Isaiah <isaiah@localhost.localdomain>
2020-05-17 14:56:52 -0400

    Added basic list view functionality for chores

--- lib/main.dart
+++ lib/main.dart
@@ -13,29 +13,71 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Chore Boss'),
-        ),
-        body: ListView(
-          children: [
-            Center(
-              child: Text(
-                'Today\'s Chores',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 35,
-                ),
-              ),
-            ),
-          ], // Children
-        ),
-        floatingActionButton: FloatingActionButton(
-          //onPressed: () => setState(() => _count++), TODO: Fill in this action to add new chore
-          tooltip: 'New Chore',
-          child: const Icon(Icons.add),
-        ),
-      ),
+      home: Chores(),
     );
   }
+}
+
+class ChoresState extends State<Chores> {
+  final _chores = <String>['Make bed', 'Clean room', 'Make breakfast'];
+  final Set<String> _done = Set<String>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  void _pushDone() {
+    Navigator.of(context).push(
+      
+    );
+  }
+
+  Widget _buildRow(String choreDescr) {
+    final bool alreadyDone = _done.contains(choreDescr);
+    return ListTile(
+      title: Text(
+        choreDescr,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadyDone ? Icons.check_box_outline_blank : Icons.check_box,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadyDone) {
+            _done.remove(choreDescr);
+          } else {
+            _done.add(choreDescr);
+          }
+        });
+      },
+    );
+  }
+
+  Widget _buildChores() {
+    var _choreWidgets = <Widget>[];
+
+    for (String _choreDescr in _chores) {
+      _choreWidgets.add(_buildRow(_choreDescr));
+    }
+
+    return ListView(
+      children: _choreWidgets,
+    ); // ListView
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Today\'s Chores'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushDone),
+        ],
+      ),
+      body: _buildChores(),
+    ); // Scaffold
+  }
+}
+
+class Chores extends StatefulWidget {
+  @override
+  ChoresState createState() => ChoresState();
 }
